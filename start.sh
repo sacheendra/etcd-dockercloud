9147e3571d8105ea6ae7f9f292931d558a61b957
Sacheendra Talluri <sacheendra.t@gmail.com>
2016-03-28 12:21:48 +0530

    sixed start.sh to correctly add other hosts

--- start.sh
+++ start.sh
@@ -9,21 +9,18 @@ elif [ -z "$MAX_SERVERS" ]; then
 fi
 
 
-INTIAL_CLUSTER=""
-for i in $( eval echo {1..$MAX_SERVERS});do
-   INTIAL_CLUSTER="$INTIAL_CLUSTER $DOCKERCLOUD_CONTAINER_HOSTNAME=http://$DOCKERCLOUD_CONTAINER_HOSTNAME:2380,"
+INITIAL_CLUSTER=""
+for i in $( seq 1 $MAX_SERVERS );do
+   INITIAL_CLUSTER="$INITIAL_CLUSTER$DOCKERCLOUD_SERVICE_HOSTNAME-$i=http://$DOCKERCLOUD_SERVICE_HOSTNAME-$i:2380,"
 done
 
 RUN_COMMAND="exec /bin/etcd -name $DOCKERCLOUD_CONTAINER_HOSTNAME \
-  -advertise-client-urls https://$DOCKERCLOUD_CONTAINER_HOSTNAME:2379 \
-  -listen-client-urls https://0.0.0.0:2379 \
+  -advertise-client-urls http://$DOCKERCLOUD_CONTAINER_FQDN:2379 \
+  -listen-client-urls http://0.0.0.0:2379 \
   -initial-advertise-peer-urls http://$DOCKERCLOUD_CONTAINER_HOSTNAME:2380 \
   -listen-peer-urls http://0.0.0.0:2380 \
   -initial-cluster-token $CLUSTER_NAME \
-  -initial-cluster $INTIAL_CLUSTER \
-  -trusted-ca-file /etcd-init/ca.crt \
-  -cert-file /etcd-init/cert_chain.crt \
-  -key-file /etcd-init/discovery1_dec.key \
+  -initial-cluster $INITIAL_CLUSTER \
   -data-dir /data "
 
 echo $RUN_COMMAND
